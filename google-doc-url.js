93794597fead500415eed88f5ad8917492506dea
AtilioAgostino <110839022+AtilioAgostino@users.noreply.github.com>
2022-09-14 13:36:08 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/16v5GZsM4X_tEiTC9QRdgODKfu182rA19oHJWZYUuqsQ/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-0Y7qN2liOicJm4noqYAjWNC-cCL7Th1jJ8hUJtUVRw/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
